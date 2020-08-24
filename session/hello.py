b13340855e7050157e842463360e3adcc58b13af
maihw <704687027@qq.com>
2020-08-24 11:45:19 +0800

    flask

---
+++ session/hello.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import abort, redirect, url_for
+from flask import render_template
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+@app.route('/login')
+def login():
+    abort(401)
+    this_is_never_executed()
+
+@app.route('/maihw/')
+def maihw():
+    return "i love you"
+
+@app.errorhandler(401)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404

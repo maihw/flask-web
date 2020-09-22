f7006c5a4db964e99f07fbf6025b355ced307fef
maihw <704687027@qq.com>
2020-09-22 17:32:26 +0800

    direct,abort,session,flash,request

---
+++ second-flask/redirect/hello.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from flask import abort, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+@app.route('/login')
+def login():
+    abort(401)
+    this_is_never_executed()
+
+@app.errorhandler(401)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
+@app.errorhandler(404)
+def not_found(error):
+    resp = make_response(render_template('error.html'), 404)
+    resp.headers['X-Something'] = 'A value'
+    return resp
